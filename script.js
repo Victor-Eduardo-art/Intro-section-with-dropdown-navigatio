01ff346a7f57a39456b7d041866fd621b819a5aa
Victor Eduardo <vcladara@gmail.com>
2022-06-26 01:48:39 -0300

    feat: finalizei a página para desktop

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function iniciar () {
-    console.log('oi')
+function ativarDesativarSubmenu () {
+
+    for (let i = 0; i < document.querySelectorAll('.ctr-submenu .botao-submenu').length; i++) {
+        
+        let botao = document.querySelectorAll('.ctr-submenu .botao-submenu')[i]
+        
+        botao.addEventListener('click', () => {
+            const submenu = document.querySelectorAll('.ctr-submenu .submenu')[i]
+            const icone = document.querySelectorAll('.ctr-submenu .botao-submenu img')[i] // vai receber o ícone que está no botão 
+            
+            submenu.classList.toggle('none')
+
+            if (submenu.classList.contains('none') === true) {
+                icone.src = 'images/icon-arrow-down.svg'
+            } else if (submenu.classList.contains('none') === false) {
+                icone.src = 'images/icon-arrow-up.svg'
+            }
+        })
+    }
 }
 
-window.addEventListener('load', iniciar)
+function gerenciarFuncoes () {
+    ativarDesativarSubmenu()
+}
+
+window.addEventListener('load', gerenciarFuncoes)
